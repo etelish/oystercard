b2c7ae83e81fe08454d21d0705ef85851a004d90
etelish <etelisha@googlemail.com>
2019-08-27 15:59:51 +0100

    added test and method for allowing oystercard top_up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,8 @@ describe Oystercard do
   it "has a balance of zero" do
   expect(subject.balance).to eq(0)
   end
+
+  it 'allows balance top_up' do
+  expect(subject).to respond_to(:top_up).with(1).argument
+  end
 end
